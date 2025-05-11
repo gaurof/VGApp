c96d0df08d0f27562c75ba9a9b03b92754c8c43c
George Daurov <daurovgeorge@yandex.ru>
2025-05-11 19:36:02 +0300

    Add project files.

---
+++ VGAppDb/VGAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using VGAppDb.Models;
+
+namespace VGAppDb;
+
+public class VGAppDbContext : DbContext
+{
+    public VGAppDbContext(DbContextOptions<VGAppDbContext> options)
+        : base(options)
+    {
+        Database.Migrate();
+    }
+    public DbSet<User> Users { get; set; }
+}
