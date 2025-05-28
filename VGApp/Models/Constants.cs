d5f64e1793c1bfedb654041325f012c6a3df5c35
George Daurov <daurovgeorge@yandex.ru>
2025-05-28 21:31:52 +0300

    Added Constants for admins and IdentityInitializer for creating user roles and an Admin user

---
+++ VGApp/Models/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace VGApp.Models;
+
+public static class Constants
+{
+    public const string AdminRoleName = "admin";
+    public const string DefaultRoleName = "default";
+}
---
+++ VGApp/Models/IdentityInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualBasic;
+using VGAppDb.Models;
+
+namespace VGApp.Models;
+
+public class IdentityInitializer
+{
+    public static async Task Initialize(UserManager<User> userManager, RoleManager<IdentityRole> roleManager) 
+    {
+        var adminUsername = "admin";
+        var adminPassword = "123qweasd";
+
+        if ((await roleManager.FindByNameAsync(Constants.AdminRoleName)) is null)
+            await roleManager.CreateAsync(new IdentityRole(Constants.AdminRoleName));
+
+        if ((await roleManager.FindByNameAsync(Constants.DefaultRoleName)) is null)
+            await roleManager.CreateAsync(new IdentityRole(Constants.DefaultRoleName));
+
+        if ((await userManager.FindByNameAsync(adminUsername)) is null)
+        {
+            var adminUser = new User { UserName = adminUsername };
+            var creationResult = await userManager.CreateAsync(adminUser, adminPassword);
+            if (creationResult.Succeeded) 
+                await userManager.AddToRoleAsync(adminUser, Constants.AdminRoleName);
+        }
+    }
+}
